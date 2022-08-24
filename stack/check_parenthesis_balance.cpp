6297f666a94beeed8cbc5dc1a287c7e2126d6a32
engrkashem <kashemaust@gmail.com>
2022-08-24 11:23:12 +0600

    check parentheses updated

--- stack/check_parenthesis_balance.cpp
+++ stack/check_parenthesis_balance.cpp
@@ -6,26 +6,27 @@ using namespace std;
 bool checkParenthesesBalance(string str){
     Stack<char> st;
     for(int i=0; i<str.length(); i++){
-        if(str[i]=='(' || str[i]=='{' || str[i]=='['){
+        if(st.empty()){
             st.push(str[i]);
             continue;
         }
 
-        if(str[i]==')' || str[i]=='}' || str[i]==']'){
-            if(st.empty())return false;
-            char chk=st.pop();
-            if(str[i]==')' && chk=='(')continue;
-            if(str[i]=='}' && chk=='{')continue;
-            if(str[i]==']' && chk=='[')continue;
-            return false;
+        if((str[i]==')' && st.Top()=='(')
+           || (str[i]=='}' && st.Top()=='{')
+           || (str[i]==']' && st.Top()=='[')){
+            st.pop();
+        }
+        else{
+            st.push(str[i]);
         }
     }
     if(st.empty())return true;
-    else return false;
+
+    return false;
 }
 
 int main(){
-    string str="({(())[{}]})";//({(())[{}])"; "[(])"; "[()]{}{[()()]()}";
+    string str="({(())[{}])";//"({(())[{}]})"; "({(())[{}])"; "[(])"; "[()]{}{[()()]()}";
     bool isBalance=checkParenthesesBalance(str);
     if(isBalance)cout<<"Balanced Parentheses"<<endl;
     else cout<<"Unbalanced Parentheses"<<endl;
